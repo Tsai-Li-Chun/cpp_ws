913d539facddf2f7e599aa8493142f0d94135cf5
Tsai-Li-Chun <4a237051@stust.edu.tw>
2022-05-26 17:17:53 +0800

    commit2 FC02 ZLAN6042 read DI

--- c++_modbus_project/src/main.cpp
+++ c++_modbus_project/src/main.cpp
@@ -65,18 +65,31 @@ int main(int argc, char **argv)
 	/* 建立modbus通訊結構體 */
 	modbus_t *mb;
 	/* 建立資料暫存陣列 */
-	uint16_t tab_reg[32];
+	uint8_t tab_reg[nb];
 	/* 宣告存放libmodbus-API的return值 */
 	int rc;
+	uint32_t to_sec;
+	uint32_t to_usec;
 
 	/* 初始化modbus通訊設定 */
-	mb = modbus_new_tcp("192.168.0.161",502);
+	mb = modbus_new_tcp("192.168.0.183",502);
+	rc = modbus_set_slave(mb,1);
+	std::cout << rc << " : " << modbus_strerror(errno) << std::endl;
 	/* 建立連結 */
 	rc = modbus_connect(mb);
-	// /* FC03,讀取AO暫存器,從start_address開始往後讀nb個 */
-	// rc = modbus_read_registers(mb, start_address, nb, tab_reg);
-	/* 打印錯誤狀態 */
-	std::cout << modbus_strerror(errno) << std::endl;
+	std::cout << rc << " : " << modbus_strerror(errno) << std::endl;
+
+
+	modbus_get_byte_timeout( mb ,&to_sec ,&to_usec );
+	std::cout << unsigned(to_sec) << " -- " << unsigned(to_usec) << std::endl;
+	modbus_get_response_timeout( mb ,&to_sec ,&to_usec );
+	std::cout << unsigned(to_sec) << " -- " << unsigned(to_usec) << std::endl;
+
+	/* FC02,讀取DI暫存器,從start_address開始往後讀nb個 */
+	rc = modbus_read_input_bits(mb, start_address, nb, tab_reg);
+	std::cout << rc << std::endl;
+	for( int i=0; i<rc; i++ )
+		std::cout << unsigned(tab_reg[i]) << std::endl;
 
 	/* 關閉連結 */
 	modbus_close(mb);
