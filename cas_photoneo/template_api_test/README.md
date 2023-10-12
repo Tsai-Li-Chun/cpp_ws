e70a19a0f19d27ab114789d72bdc89373a8b5c7d
Tsai,Li-chun <4a237051@stust.edu.tw>
2023-10-12 20:14:52 +0800

    add Localization-SDK function, using .ply & .plcf get target object position

--- cas_photoneo/template_api_test/README.md
+++ cas_photoneo/template_api_test/README.md
@@ -1,3 +1,6 @@
 photoneo API&SDK 測試，並建立程式範本，
 並將預設CMake compile更改成makefile compile。
+
 photoneoXiControl SDK，範本程式為使用SDK偵測XiControl有無運行。
+
+Localization SDK，範本程式為使用ply檔&plcf檔取得目標物件姿態。
--- cas_photoneo/template_api_test/src/main.cpp
+++ cas_photoneo/template_api_test/src/main.cpp
@@ -64,14 +64,81 @@ int main(int argc, char **argv)
     if (!Factory.isPhoXiControlRunning())
     {
         std::cout << "PhoXi Control Software is not running" << std::endl;
-        return 0;
     }
     else
     {
         std::cout << "PhoXi Control Software is running OK!" << std::endl;
     }
 
-	
+	std::unique_ptr<PhoLocalization> localization;
+    //Initialize localization object.
+    //Note that without proper authentication, this step will fail.
+    //See Photoneo Localization SDK manual how to authenticate your installation.
+    try 
+	{
+        localization.reset(new PhoLocalization());
+		std::cout << "PhoLocalization reset Success" << std::endl;
+    }
+	catch (const AuthenticationException & ex)
+	{
+        std::cout << ex.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    try
+	{
+        SceneSource scene;
+        //Set scene where to look for an object. Possible scene sources are:
+        //Photoneo device (Photoneo PhoXi 3D Scanner, Photoneo MotionCam 3D) or file camera (e.g., "InstalledExamples-basic-example"):
+        //     scene = SceneSource::PhoXi("Device ID");
+        //*.praw or *.ply files created by Photoneo PhoXi Control from 3D scans:
+        //     scene = SceneSource::File("path to praw / ply file");
+        scene = SceneSource::File("template_api_test1.ply");
+        localization->SetSceneSource(scene);
+		std::cout << "SetSceneSource Success" << std::endl;
+    }
+	catch (const PhoLocalizationException & ex)
+	{
+        std::cout << "SetSceneSource Error: " << ex.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    try
+	{
+        //Load PLCF file containing a preprocessed model, localization settings, and stopping criteria.
+        localization->LoadLocalizationConfiguration("template_api_test.plcf");
+		std::cout << "LoadLocalizationConfiguration Success" << std::endl;
+    }
+	catch (const IOException & ex)
+	{
+        std::cout << "Error loading plcf file: " << ex.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    //See ProgrammaticSettings example for adjusting localization settings and setting stopping criteria.
+
+    AsynchroneResultQueue queue;
+    try
+	{
+        queue = localization->StartAsync();
+        //If the scene and model were correctly set, calling StartAsync() starts a localization loop.
+        //Localized poses will begin to appear in the queue.
+        //The loop will run until any of the stopping criteria stored in the PLCF file is met.
+        //If no stopping criteria are set, the loop will run indefinitely or until localization->StopAsync() is called.
+    }
+	catch (const PhoLocalizationException & ex)
+	{
+        std::cout << "Error starting localization: " << ex.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    TransformationMatrix4x4 result;
+    std::cout << "Localization results:" << std::endl;
+    while (queue.GetNext(result))
+	{
+        std::cout << result << std::endl;
+    }
+    std::cout << "Localization finished" << std::endl;
 
 	// /* main loop */
 	// while(1)
